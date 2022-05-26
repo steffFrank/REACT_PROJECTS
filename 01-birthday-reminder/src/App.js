5c4eba1cfa877a8cdf94b78753c93f9ddad7f081
steffFrank <--replace-all>
2022-05-26 23:23:14 +0200

    feat: Add handleClick

--- 01-birthday-reminder/src/App.js
+++ 01-birthday-reminder/src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import { List } from "./List";
+import data from "./data";
 
 
 export const App = () => {
+  const [people, setPeople] = useState(data);
+
+  const handleClick = () => {
+    setPeople([]);
+  }
+
   return (
-    <>
-      <List />
-    </>
+    <main>
+      <div className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List />
+        <button onClick={handleClick} className="btn">clear all</button>
+      </div>
+      
+    </main>
   )
 }
