15c0df640617ff9faf6474775838e38018eaeb8b
steffFrank <--replace-all>
2022-05-27 08:30:39 +0200

    feat: Fetch the data

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
-
+import React, {useEffect, useState} from "react";
+import Tours from "./Components/Tours";
 
 export const App = () => {
+
+  const url = "https://course-api.com/react-tours-project";
+
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const FetchData = async (url) => {
+    const response = await fetch(url);
+    const fetchedData = await response.json();
+    setData(fetchedData);
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    FetchData(url);
+  }, [])
   return (
     <section>
-      
+      <Tours />
     </section>
   )
 }
