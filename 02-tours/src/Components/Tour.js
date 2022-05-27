3ec30eb00b145c847d16f06212727294874044fb
steffFrank <--replace-all>
2022-05-27 09:05:28 +0200

    feat: Add each single tour

--- 02-tours/src/Components/Tour.js
+++ 02-tours/src/Components/Tour.js
@@ -1,8 +1,19 @@
 import React from "react";
 
-export const Tour = () => {
+export const Tour = ({name, id, info, image, price}) => {
+
     return (
-        <>
-        </>
+        <div className="single-tour">
+            <img src={image} alt={name} />
+            <footer>
+                <div className="tour-info">
+                    <h4>{name}</h4>
+                    <h4 className="tour-price">${price}</h4>
+                </div>
+                <p>{info}</p>
+                <button className="delete-btn">not interested</button>
+            </footer>
+            
+        </div>
     )
 }
