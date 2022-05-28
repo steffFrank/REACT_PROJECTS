db1c58e6e4c0ee4f4386cb88184cb089681a2cfe
steffFrank <--replace-all>
2022-05-28 10:44:57 +0200

    feat: Add a template review

--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -3,7 +3,27 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 export const Review = () => {
-  return <h2>review component</h2>;
+
+  const [person, setPerson] = useState(people[0]);
+
+  return (
+    <div className='review'>
+      <div className='img-container'>
+        <img className='person-img' src={person.image} alt={person.name} />
+        <div className='quote-icon'>
+          <FaQuoteRight />
+        </div>
+      </div>
+      <h4 className='author'>{person.name}</h4>
+      <p className='job'>{person.job}</p>
+      <p className='info'>{person.text}</p>
+      <div>
+        <button className='prev-btn'><FaChevronLeft /></button>
+        <button className='next-btn'><FaChevronRight /></button>
+      </div>
+      <button className='random-btn'>surprise me</button>
+    </div>
+  );
 };
 
 
