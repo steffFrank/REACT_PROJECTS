0af93b477698bc58b7ce1b438108590e23438829
steffFrank <--replace-all>
2022-05-28 10:49:30 +0200

    style: Adjust the position of the container review

--- 03-reviews/src/App.js
+++ 03-reviews/src/App.js
@@ -4,13 +4,15 @@ import { Review } from './Review';
 export const App = ()  => {
   return (
     <main>
-      <div className='title'>
-        <h2>our reviews</h2>
-        <div className='underline'></div>   
-      </div>
-      <div className='container'>
-        <Review />
-      </div>
+      <section>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>   
+        </div>
+        <div className='container'>
+          <Review />
+        </div>
+      </section>
     </main>
   )
 }
