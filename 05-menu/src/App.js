e1b8e2b58ce9b418e72d140418d1f8e8d72e9061
steffFrank <--replace-all>
2022-05-29 09:37:22 +0200

    feat: Pass menu items through props to Menu component

--- 05-menu/src/App.js
+++ 05-menu/src/App.js
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
-import { Menu }  from './Menu';
-import { Categories } from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import { Menu }  from "./Menu";
+import { Categories } from "./Categories";
+import items from "./data";
+
+
 
 export const App = () =>  {
+
+  const [menus, setMenus] = useState(items);
+
+
   return (
-    <main className='menu'>
-      <div className='title'>
+    <main className="menu">
+      <div className="title">
         <h2>our menu</h2>
-        <div className='underline'></div>
+        <div className="underline"></div>
       </div>
       <Categories />
-      <Menu />
+      <section className="section-center">
+        {menus.map(menu => {
+          return (
+            <Menu {...menu} />
+          )
+        })}
+      </section>
     </main>
   )
 }
