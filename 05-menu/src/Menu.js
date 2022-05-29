cf984c3b1d64cc367d0c68c5f561052e385c83bf
steffFrank <--replace-all>
2022-05-29 09:41:17 +0200

    feat: Create Menu component with list of items

--- 05-menu/src/Menu.js
+++ 05-menu/src/Menu.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React from "react";
 
-export const Menu = () => {
-  return <h2>menu component</h2>;
+export const Menu = ({title, img, price, desc}) => {
+  return (
+      <article className="menu-item">
+        <img className="photo" src={img} alt={title} />
+        <div className="item-info">
+          <header>
+            <h4>{title}</h4>
+            <p className="price">{price}</p>
+          </header>
+          <p className="item-text">{desc}</p>
+        </div>
+      </article>
+  )
 };
 
