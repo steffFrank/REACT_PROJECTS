b5a0030ac1b9b1f07c767c2025f0ab80b1a60ce4
steffFrank <--replace-all>
2022-05-29 12:32:54 +0200

    style: Add the dollar sign before price

--- 05-menu/src/Menu.js
+++ 05-menu/src/Menu.js
@@ -7,7 +7,7 @@ export const Menu = ({title, img, price, desc}) => {
         <div className="item-info">
           <header>
             <h4>{title}</h4>
-            <p className="price">{price}</p>
+            <p className="price">${price}</p>
           </header>
           <p className="item-text">{desc}</p>
         </div>
