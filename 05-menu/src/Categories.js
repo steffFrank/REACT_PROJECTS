8db78ee1385486484c26a64e2c61bb4b00145e32
steffFrank <--replace-all>
2022-05-29 12:32:12 +0200

    feat: Use the filter function props in the category component to filter the menu

--- 05-menu/src/Categories.js
+++ 05-menu/src/Categories.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 
-export const Categories = ({categories}) => {
+export const Categories = ({categories, filterMenuByCategory}) => {
   return (
     <nav className='btn-container'>
       {categories.map((category, index) => {
         return (
           <ul>
-            <li className="filter-btn" key={index}>{category}</li>
+            <li key={index}><button className="filter-btn" onClick={() => filterMenuByCategory(category)}>{category}</button></li>
           </ul>
         )
       })}
