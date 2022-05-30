c45c9f10a01cafa27baa4948e10276bfd02a103d
steffFrank <--replace-all>
2022-05-30 22:58:02 +0200

    feat: Structure the Review component

--- 05-slider/src/Review.js
+++ 05-slider/src/Review.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { FaQuoteRight } from "react-icons/fa";
 
 
-export const Review = () => {
+export const Review = ({image, name, title, quote}) => {
     return (
         <article>
-            
+            <img className="person-img" src={image} alt={name} />
+            <h4>{name}</h4>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <div className="icon"><FaQuoteRight /></div>
         </article>
     )
 }
