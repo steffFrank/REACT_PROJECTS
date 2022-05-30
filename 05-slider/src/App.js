99e021cbc43a9da0d828b8bbec26c4cf9008a75d
steffFrank <--replace-all>
2022-05-30 22:46:12 +0200

    feat: Add the structure of the components

--- 05-slider/src/App.js
+++ 05-slider/src/App.js
@@ -1,10 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 
 
 export const App = () =>  {
-  return <h2>slider project</h2>;
+  return (
+    <main className='section'>
+      <div className="title">
+        <h2><span>/</span>reviews</h2>
+      </div>
+    </main>
+  )
 }
 
---
+++ 05-slider/src/Review.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const Review = () => {
+    return (
+        <article>
+            
+        </article>
+    )
+}
