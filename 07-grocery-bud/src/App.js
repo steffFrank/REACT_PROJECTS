f8f64e66b8011ab9e1248e57e140f314edb8ced2
steffFrank <--replace-all>
2022-06-01 09:56:24 +0200

    feat: Add structure of the List component

--- 07-grocery-bud/src/App.js
+++ 07-grocery-bud/src/App.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { List } from "./List";
 import { Alert } from "./Alert";
 import { Form } from "./Form";
 
 
 export const App = () => {
+
+  const [items, setItems] = useState([]);
+  
   return (
     <div className="section-center">
       <div className="grocery-form">
         <Alert />
         <h3>grocery bud</h3>
         <Form />
-        <List />
+        <List items={items}/>
       </div>
      
     </div>
--- 07-grocery-bud/src/List.js
+++ 07-grocery-bud/src/List.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { FaTrash, FaEdit } from "react-icons/fa";
 
 
-export const List = () => {
+export const List = ({items}) => {
   return (
-    <h3>List Component</h3>
+    <article className="grocery-container">
+      {items.map(item => {
+        return (
+          <div key={item.id} className="grocery-item">
+            <p className="title">{item.name}</p>
+            <button className="edit-btn"><FaEdit /></button>
+            <button className="delete-btn"><FaTrash /></button>
+          </div>
+        )
+      })}
+      <button className="clear-btn">clear items</button>
+    </article>
   )
 }
