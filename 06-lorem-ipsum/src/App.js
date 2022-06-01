6dff6a548d05dbcdd1dbd7995e77b983011ee2c2
steffFrank <--replace-all>
2022-06-01 08:53:57 +0200

    feat: Add functions and pass it through props to From

--- 06-lorem-ipsum/src/App.js
+++ 06-lorem-ipsum/src/App.js
@@ -1,14 +1,40 @@
 import React, { useState } from "react";
 import data from "./data";
-import {Form} from "./Form";
+import { Form } from "./Form";
 
 export const App = () => {
   
   const maxLength = data.length;
+  const [index, setIndex] = useState(0);
+  const [isGenerate, setIsGenerate] = useState(false);
+
+  const lorems = data.slice(0, index);
+
+  const generateLorem = (e) => {
+    e.preventDefault();
+    setIndex(e.target.value);
+    setIsGenerate(false);
+
+  }
+
+  const showResult = (e) => {
+    e.preventDefault();
+    setIsGenerate(true);
+  }
+
   return (
     <section className="section-center">
       <h3>tired of boring lorem ipsum?</h3>
-      <Form maxLength={maxLength}/>
+      <Form maxLength={maxLength}
+            showResult={showResult}
+            generateLorem={generateLorem} />
+      {isGenerate && <div className="result">
+        {lorems.map((lorem, index) => {
+          return (
+            <p key={index}>{lorem}</p>
+          )
+        })}
+      </div>}
     </section>
     )
 }
